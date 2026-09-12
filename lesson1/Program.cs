e019d7344502cfade2355a41666020f96d700cc6
rakdav <rakdav@inbox.ru>
2026-09-12 13:48:02 +0200

    Добавьте файлы проекта.

---
+++ lesson1/Program.cs
@@ -0,0 +1,25 @@
+﻿//Console.Write("Введите ваше имя:");
+//string name = Console.ReadLine();
+//Console.WriteLine("Меня зовут "+name);
+//Console.WriteLine("Меня зовут {0}",name);
+//Console.WriteLine($"Меня зовут {name}");
+
+//Console.Write("Введите x:");
+//int x= int.Parse(Console.ReadLine());
+//Console.Write("Введите y:");
+//int y= Convert.ToInt32(Console.ReadLine());
+//int s = x + y;
+//Console.WriteLine($"{x}+{y}={s}");
+
+//float x = 3.7F;
+//double y = Math.Pow(x, 7);
+
+//Console.Write("Введите угол в градусах:");
+//double angle = double.Parse(Console.ReadLine());
+//double y=Math.Sin(angle*Math.PI/180);
+//Console.WriteLine($"y={y:f2}");
+
+//Console.Write("Введите степень числа:");
+//double x = double.Parse(Console.ReadLine());
+//double y=Math.Exp(7*x);
+//Console.WriteLine($"y={y:f2}");
