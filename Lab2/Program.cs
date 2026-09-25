8e5fa72ebf9501cac54ba38f067fed751bfc72ca
rakdav <rakdav@inbox.ru>
2026-09-25 18:09:32 +0200

    les3

---
+++ Lab2/Program.cs
@@ -0,0 +1,61 @@
+﻿//bool A = true;
+//bool B = false;
+//bool C = false;
+//Console.WriteLine(!A||A&&(B||C));
+//try
+//{
+//    Console.Write("Введите x:");
+//    double x = double.Parse(Console.ReadLine());
+//    Console.Write("Введите y:");
+//    double y = double.Parse(Console.ReadLine());
+//    Console.WriteLine(x*x+y*y<=4);
+//    Console.WriteLine((x>=0)||(y*y!=4));
+//    Console.WriteLine((x >= 0) && (y * y != 4));
+//    Console.WriteLine((x*y!=0)&&(y>x));
+
+//}
+//catch(Exception e)
+//{
+//    Console.WriteLine(e.Message);
+//}
+
+//try
+//{
+//    Console.Write("Введите x:");
+//    double x = double.Parse(Console.ReadLine());
+//    double y;
+//    if (x > 0) y = Math.Sin(x) * Math.Sin(x);
+//    else y = 1 - 2 * Math.Sin(x * x);
+//    Console.WriteLine($"y={y:F2}");
+//}
+//catch (Exception e)
+//{
+//    Console.WriteLine(e.Message);
+//}
+
+try
+{
+    Console.Write("Введите a:");
+    double a = double.Parse(Console.ReadLine());
+    Console.Write("Введите b:");
+    double b = double.Parse(Console.ReadLine());
+    Console.Write("Введите c:");
+    double c = double.Parse(Console.ReadLine());
+    double d = b * b - 4 * a * c;
+    if (d > 0)
+    {
+        double x1 = (-b + Math.Sqrt(d)) / (2 * a);
+        double x2 = (-b - Math.Sqrt(d)) / (2 * a);
+        Console.WriteLine($"x1={x1:F2} x2={x2:F2}");
+    }
+    else if (d == 0)
+    {
+        double x = -b/ (2 * a);
+        Console.WriteLine($"x={x:F2}");
+    }
+    else Console.WriteLine();
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
--- lesson1/Program.cs
+++ lesson1/Program.cs
@@ -24,3 +24,94 @@
 //double y=Math.Exp(7*x);
 //Console.WriteLine($"y={y:f2}");
 
+//Console.WriteLine($"{Math.PI:F2}");
+//Console.WriteLine($"{Math.E:F1}");
+//int a = int.Parse(Console.ReadLine());
+//Console.WriteLine($"Вы ввели число {a}");
+
+//int a = int.Parse(Console.ReadLine());
+//Console.WriteLine($"{a} - Вот какое число вы ввели");
+//Console.WriteLine("1 13 49");
+//Console.WriteLine("7\t15\t100");
+//try
+//{
+//    Console.Write("Введите a:");
+//    double a = double.Parse(Console.ReadLine());
+//    Console.Write("Введите b:");
+//    double b = double.Parse(Console.ReadLine());
+//    Console.Write("Введите c:");
+//    double c = double.Parse(Console.ReadLine());
+//    double d = (-b + Math.Sqrt(b * b - 4 * a * c)) / (2 * a);
+//    Console.WriteLine($"{d:F2}");
+//}
+//catch(Exception ex)
+//{
+//    Console.WriteLine(ex.Message);
+//}
+//try
+//{
+//    Console.Write("Введите радиус:");
+//    double R = double.Parse(Console.ReadLine());
+//    double D = 2 * R;
+//    Console.WriteLine($"Диаметр окружности:{D:F2}");
+//}
+//catch(Exception ex)
+//{
+//    Console.WriteLine(ex.Message);
+//}
+
+//try
+//{
+//    Console.Write("Введите x:");
+//    double x = double.Parse(Console.ReadLine());
+//    Console.Write("Введите y:");
+//    double y = double.Parse(Console.ReadLine());
+//    double z = (x+((2+y)/x*x)) / (y+(1/Math.Sqrt(x*x+10)));
+//    Console.WriteLine($"z={z:F2}");
+//    double q = 2.8 * Math.Sin(x) + Math.Abs(y);
+//    Console.WriteLine($"q={z:F2}");
+//}
+//catch (Exception ex)
+//{
+//    Console.WriteLine(ex.Message);
+//}
+
+//try
+//{
+//    //Console.Write("Введите количество сантиметров:");
+//    //int sm=int.Parse(Console.ReadLine());
+//    //int m = sm / 100;
+//    //Console.WriteLine($"Полных метров {m}");
+//    //Console.Write("Введите количество секунд:");
+//    //int n = int.Parse(Console.ReadLine());
+//    //int hour = n / 3600;
+//    //int minute = n % 3600/60;
+//    //int second = n%3600%60;
+//    //Console.WriteLine($"{hour}:{minute}:{second}");
+//    Console.Write("Введите чытерехзначное число:");
+//    int n= int.Parse(Console.ReadLine());
+//    int a = n % 10;
+//    int b= n % 100/10;
+//    int c = n % 1000/100;
+//    int d = n / 1000;
+//    int s = a + b + c + d;
+//    Console.WriteLine(s);
+//}
+//catch (Exception ex)
+//{
+//    Console.WriteLine(ex.Message);
+//}
+
+try
+{
+    Console.Write("Введите номер места: ");
+    int place = int.Parse(Console.ReadLine());
+    int tier = place / 120 + 1;
+    int section = place % 120 / 15;
+    int ps = place % 120 % 15 + 1;
+    Console.WriteLine($"Ярус: {tier}  Секция: {section}  Место в секции: {ps}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
